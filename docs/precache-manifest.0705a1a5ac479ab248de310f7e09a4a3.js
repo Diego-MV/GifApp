d8130f093ce4f683d78015a957d52dcec1d8b9a2
Diego M <diegomoralesval@gmail.com>
2020-08-20 14:14:49 -0400

    index.html actualizado4

---
+++ docs/precache-manifest.0705a1a5ac479ab248de310f7e09a4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3d754387fbc22be1edd09f88104c47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029f5675682cb68683cc",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "029f5675682cb68683cc",
+    "url": "/static/js/2.d7361351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7361351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992294d85ad598bfa125",
+    "url": "/static/js/main.c3dba4ec.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
