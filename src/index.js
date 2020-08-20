ffc95a8beeee5699403a48f90a4c7415455eac47
Diego M <diegomoralesval@gmail.com>
2020-08-20 14:18:03 -0400

    index.html actualizado5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GifExpertApp from './components/GifExpertApp';
-import 'bootstrap/dist/css/bootstrap.css';
-
 ReactDOM.render(
   <GifExpertApp />,
   document.getElementById('root')
